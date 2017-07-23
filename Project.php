53cdfd4638ce015e21b4b454bfc09039cbea2542
Joseph Mancuso <joseph@orderinghero.com>
2017-07-22 23:07:04 -0400

    updated project for version 2 and made it psr-4 autoloader compliant

---
+++ Project.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Mira\Project;
+
+class Project
+{
+    public $providers;
+
+    public static function config($config_name = false)
+    {
+        if (!$config_name) {
+            return include $_SERVER['DOCUMENT_ROOT']."/config/config.php";
+        } else {
+            $config = include $_SERVER['DOCUMENT_ROOT']."/config/config.php";
+            return $config[$config_name];
+        }
+    }
+
+    public function setProviders($providers)
+    {
+        $this->providers = $providers;
+        return true;
+    }
+}
